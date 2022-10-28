7088c8a161e2519e8a6b183215a223437f74f985
ArchiesSingh <59329806+Archies13Singh@users.noreply.github.com>
2022-10-28 18:09:30 +0530

    Add files via upload

---
+++ day31/steps/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <textarea name="" id="" cols="30" rows="10" v-model.trim="message"></textarea>
+    
+  </div>
+</template>
+ 
+<script>
+export default {
+data(){
+  return {
+    title : "Forms and Data",
+    message :'Archies      aSDasda asdasd as    sdasdasd'
+ 
+  }
+}
+}
+</script>
+ 
+<style></style>
+ 
+<!-- http://p.ip.fi/9V2E -->
